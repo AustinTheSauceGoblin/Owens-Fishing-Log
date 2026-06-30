49d7e5cf627ed4e9ea523c43704309a3d69cc8f4
AustinTheSauceGoblin <austinrkennedy@icloud.com>
2026-06-30 17:40:49 -0400

    Update config.js
    synced with owen's sheet

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const CONFIG = {
 
   // Paste your Google Apps Script Web App URL here.
   // You get this after deploying the script — see SETUP.md step 6.
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbztE5O15Qwcb0HcuCzXEgOgwINTJefVQCsODEV00LCkHgXIVyeKx6pjRT69qOjuk3tE/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzsUnkPvzvW7zIQVhKiF1mDKPaMUX7VRz2qh3R5F-LkJVIKw-G_Oxj_rSfSc_ZFQeat/exec",
 
   // Your name or handle — shown at the top of your log.
-  OWNER_NAME: "Austin's Fishing Log",
+  OWNER_NAME: "Owen's Fishing Log",
 
 };
